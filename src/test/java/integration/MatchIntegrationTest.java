1e0ded07bae16896ee6c64b29ea50d23d388f90c
Abraham10000 <99530526+Abraham10000@users.noreply.github.com>
2023-02-01 18:17:30 +0300

    "add : healthcontrollerIT and addgoalsIT"

--- src/test/java/integration/MatchIntegrationTest.java
+++ src/test/java/integration/MatchIntegrationTest.java
@@ -2,29 +2,47 @@ package integration;
 
 import app.foot.FootApi;
 import app.foot.controller.rest.*;
+import app.foot.service.MatchService;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.type.CollectionType;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
 
-import java.time.Instant;
+import java.io.UnsupportedEncodingException;
+import java.util.ArrayList;
 import java.util.List;
 
+import static IntegrationTestUtils.AddGoalsInMtachUtils.PlayerScorer1;
+import static IntegrationTestUtils.AddGoalsInMtachUtils.PlayerScorer2;
+import static IntegrationTestUtils.MatchOneUtils.expectedMatch1;
+import static IntegrationTestUtils.MatchThreeUtils.expectedMatch3;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static IntegrationTestUtils.MatchTwooUtils.expectedMatch2;
 
 @SpringBootTest(classes = FootApi.class)
 @AutoConfigureMockMvc
 class MatchIntegrationTest {
     @Autowired
     private MockMvc mockMvc;
+
+    MatchService service = mock(MatchService.class);
     private final ObjectMapper objectMapper = new ObjectMapper()
             .findAndRegisterModules();  //Allow 'java.time.Instant' mapping
+
 
     @Test
     void read_match_by_id_ok() throws Exception {
@@ -39,68 +57,50 @@ class MatchIntegrationTest {
         assertEquals(expectedMatch2(), actual);
     }
 
-    private static Match expectedMatch2() {
-        return Match.builder()
-                .id(2)
-                .teamA(teamMatchA())
-                .teamB(teamMatchB())
-                .stadium("S2")
-                .datetime(Instant.parse("2023-01-01T14:00:00Z"))
-                .build();
+    @Test
+    void get_all_matches_OK() throws Exception {
+        MockHttpServletResponse response = mockMvc.perform(get("/matches"))
+                .andExpect(status().isOk())
+                .andReturn()
+                .getResponse();
+        List<Match> actual = convertFromHttpResponse(response);
+        Match match3 = expectedMatch3();
+        Match match2 = expectedMatch2();
+        Match match1 = expectedMatch1();
+        assertEquals(HttpStatus.OK.value(), response.getStatus());
+        assertTrue(actual.containsAll(List.of(
+                match1,
+                match2,
+                match3)));
     }
 
-    private static TeamMatch teamMatchB() {
-        return TeamMatch.builder()
-                .team(team3())
-                .score(0)
-                .scorers(List.of())
-                .build();
+    @Test
+    public void addMatchGoalsById_OK() throws Exception {
+        int matchId = 3;
+        ArrayList<PlayerScorer> scorers = new ArrayList<>();
+        scorers.add(PlayerScorer1());
+        scorers.add(PlayerScorer2());
+
+        int exceptedGoals = scorers.size();
+        String requestBody = scorers.toString();
+
+        ResultActions response = mockMvc.perform(post("/matches/{matchId}/goals", matchId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(new ObjectMapper().writeValueAsString(scorers)));
+        response.andExpect(status().isOk());
+        Match match = new ObjectMapper().findAndRegisterModules().readValue(
+                response.andReturn().getResponse().getContentAsString(), Match.class);
+        match.getTeamA().setScore(exceptedGoals);
+        assertEquals(match.getId(), matchId);
+        assertEquals(exceptedGoals, match.getTeamA().getScore());
     }
 
-    private static TeamMatch teamMatchA() {
-        return TeamMatch.builder()
-                .team(team2())
-                .score(2)
-                .scorers(List.of(PlayerScorer.builder()
-                                .player(player3())
-                                .scoreTime(70)
-                                .isOG(false)
-                                .build(),
-                        PlayerScorer.builder()
-                                .player(player6())
-                                .scoreTime(80)
-                                .isOG(true)
-                                .build()))
-                .build();
-    }
-
-    private static Team team3() {
-        return Team.builder()
-                .id(3)
-                .name("E3")
-                .build();
-    }
-
-    private static Player player6() {
-        return Player.builder()
-                .id(6)
-                .name("J6")
-                .isGuardian(false)
-                .build();
-    }
-
-    private static Player player3() {
-        return Player.builder()
-                .id(3)
-                .name("J3")
-                .isGuardian(false)
-                .build();
-    }
-
-    private static Team team2() {
-        return Team.builder()
-                .id(2)
-                .name("E2")
-                .build();
+    private List<Match> convertFromHttpResponse(MockHttpServletResponse response)
+            throws JsonProcessingException, UnsupportedEncodingException {
+        CollectionType matchListType = objectMapper.getTypeFactory()
+                .constructCollectionType(List.class, Match.class);
+        return objectMapper.readValue(
+                response.getContentAsString(),
+                matchListType);
     }
 }
