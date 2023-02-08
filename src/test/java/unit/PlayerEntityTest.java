697781988c76a414880865119ea30ccf29eba85c
Abraham10000 <99530526+Abraham10000@users.noreply.github.com>
2023-02-08 14:17:34 +0300

    fix : PlayerEntityTest

--- src/test/java/unit/PlayerEntityTest.java
+++ src/test/java/unit/PlayerEntityTest.java
@@ -1,5 +1,6 @@
 package unit;
 import app.foot.controller.rest.Player;
+import app.foot.repository.MatchRepository;
 import app.foot.repository.TeamRepository;
 import app.foot.repository.entity.PlayerEntity;
 import app.foot.repository.entity.TeamEntity;
@@ -9,6 +10,8 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.Test;
 import app.foot.repository.PlayerRepository;
 
+import java.util.Optional;
+
 import static IntegrationTestUtils.MatchOneUtils.player1;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.mock;
@@ -17,28 +20,36 @@ import static org.mockito.Mockito.when;
 @Slf4j
 public class PlayerEntityTest {
     private TeamRepository teamRepository = mock(TeamRepository.class);
-    private PlayerMapper playerMapper = mock(PlayerMapper.class);
+    private MatchRepository matchRepository = mock(MatchRepository.class);
+
+    private PlayerRepository playerRepository = mock(PlayerRepository.class);
+    private PlayerMapper playerMapper = new PlayerMapper(matchRepository , playerRepository , teamRepository);
 
     @Test
     public void toEntityTest() {
         Player player = player1();
-        TeamEntity team = TeamEntity.builder().id(1).name("E1").build();
+
+        TeamEntity team = TeamEntity.builder()
+                .id(1)
+                .name("E1")
+                .build();
         PlayerEntity expectedPlayerEntity = PlayerEntity.builder()
                 .id(1)
                 .name("J1")
-                .team(team)
+                .team(TeamEntity.builder()
+                        .id(team.getId())
+                        .name(team.getName())
+                        .build())
                 .guardian(false)
                 .build();
-
-        when(teamRepository.findByName("E1")).thenReturn(team);
-
-        app.foot.model.Player playerToEntity = app.foot.model.Player.builder()
+        when(teamRepository.findByName("E1")).thenReturn(Optional.of(team).get());
+        PlayerEntity actualPlayerEntity = playerMapper.toEntity(app.foot.model.Player.builder()
                 .id(1)
                 .isGuardian(false)
                 .name("J1")
-                .teamName("E1").build();
+                .teamName("E1")
+                .build());
 
-        PlayerEntity actualPlayerEntity = playerMapper.toEntity(playerToEntity);
 
         assertEquals(expectedPlayerEntity, actualPlayerEntity);
     }
